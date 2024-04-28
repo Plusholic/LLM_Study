c8627dfb0dad971617fea07212ba50bf0d689d53
Plusholic <bm99093@naver.com>
2024-04-28 23:25:16 +0900

    revise

--- swift.py
+++
@@ -1,9 +0,0 @@
-import json
-
-class PythonClass:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    def to_json(self):
-        return json.dumps(self.__dict__)
